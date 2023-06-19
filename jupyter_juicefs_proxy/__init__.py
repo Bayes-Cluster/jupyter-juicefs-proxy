030b7e7a4b82867e85ef77fa788cf47f8eeafc32
TerenceLiu98 <terenceliu1012@gmail.com>
2023-06-20 02:07:33 +0800

    update

--- jupyter_juicefs_proxy/__init__.py
+++ jupyter_juicefs_proxy/__init__.py
@@ -6,35 +6,35 @@ from typing import Any, Dict
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
+def setup_gatewaye():
 
-def setup_gateway():
-    """Setup commands and icon paths and return a dictionary compatible
-    with jupyter-server-proxy.
-    """
-
-    def _get_icon_path():
-        return os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "icons", "juicefs.svg"
-        )
-
-    def _juicefs_command(port):
-        full_path = shutil.which("juicefs")
-        if not full_path:
-            raise FileNotFoundError("Can not find juicefs in $PATH")
-        # lstrip is used as a hack to deal with using paths in environments
-        # when using git-bash on windows
+    def _get_juicefs_cmd(port):
+        executable = "juicefs"
+        if not shutil.which(executable):
+            raise FileNotFoundError("Can not find juicefs in PATH")
+        
+        # Start vscode in CODE_WORKINGDIR env variable if set
+        # If not, start in 'current directory', which is $REPO_DIR in mybinder
+        # but /home/jovyan (or equivalent) in JupyterHubs
         os.environ["MINIO_ROOT_USER"] = "admin"
         os.environ["MINIO_ROOT_PASSWORD"] = "12345678"
-        return [
-            full_path,
+        cmd = [
+            executable,
             "gateway",
             "sqlite3://myjfs.db",
-            "localhost:9000"
+            "localhost:"+str(port)
         ]
 
+        return cmd
+
     return {
-        "command": _juicefs_command,
-        "timeout": 20,
+        "command": _get_juicefs_cmd,
+        "timeout": 300,
         "new_browser_tab": True,
-        "launcher_entry": {"title": "JuiceFS GW", "icon_path": _get_icon_path()},
+        "launcher_entry": {
+            "title": "JuiceFS GW",
+            "icon_path": os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "icons", "juicefs.svg"
+            ),
+        },
     }
